12a2d86c8003062529da40fe4f33004ad3598eaa
SaidBC <razzouksaid139@gmail.com>
2024-08-18 04:41:16 +0100

    setup dismiss button function

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ const submit_btn = document.querySelector('.container .left-side form button');
 const user_input = document.querySelector('.container .left-side form input');
 const success_section = document.querySelector('.success-section')
 const user_email = document.querySelector('.success-section p b')
+const dismissBtn = document.querySelector('.dismiss-btn')
 
 
 
 submit_btn.addEventListener('click', () => {
     if (/\w+\@\w+\.\w+/g.test(user_input.value)) {
         form.classList.remove('error');
-        container.remove()
+        container.classList.add('submitted')
         success_section.classList.remove('hide')
         user_email.textContent = user_input.value
     } else {
         form.classList.add('error')
     }
+})
+
+dismissBtn.addEventListener('click', () => {
+    container.classList.remove('submitted')
+    success_section.classList.add('hide')
+    form.reset()
 })
